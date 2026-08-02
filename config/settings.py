e3da8a3cf061b0ba760d6ba94ca1c51a0a944748
azzizz188044-sudo <azzizz188044@gmail.com>
2026-08-02 11:28:49 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mzm_mvu@$+5ngn-rfx&&2=u)j!^oulad!-mf=mx7migoa*9gg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
